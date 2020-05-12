47f93231f6178946cf665db3187ebc8e44592670
limseongkyu <96x60812@naver.com>
2020-05-12 21:08:31 +0900

    React_property
    기본값 지정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import ChildComponent2 from './03/ChildComponent2';
+import DefaultPropsComponent from './03/DefaultPropsComponent';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ChildComponent2 objValue={{ age: 20, name: 'limseongkyu' }} requiredStringValue="TEXT" />
+        <DefaultPropsComponent />
       </div>
     );
   }
