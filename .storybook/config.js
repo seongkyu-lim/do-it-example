203be583dcd61e102662abd65cfb3022f0d866a6
limseongkyu <96x60812@naver.com>
2020-06-15 15:30:36 +0900

    React_storybook
    added NewCounter story in stories folder and added story to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/stories/InputStory');
+  require('../src/stories/NewCounterStory');
 }
 
 configure(loadStories, module);
