6347b3f0f6e73151bf808c2c0138510492882b35
limseongkyu <96x60812@naver.com>
2020-06-15 15:36:14 +0900

    React storybook
    revise config.js for automation

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
 import { configure } from '@storybook/react';
+import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
 
 function loadStories() {
-  require('../src/stories/InputStory');
-  require('../src/stories/NewCounterStory');
+  const context = require.context('../src/stories', true, /Story\.jsx$/);
+  context.keys().forEach((srcFile) => {
+    interopRequireDefault(context(SrcFile));
+  });
 }
 
 configure(loadStories, module);
