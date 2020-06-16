cee1e0bd72fd2a1450306e4140397a0ce028a14b
limseongkyu <96x60812@naver.com>
2020-06-16 15:45:09 +0900

    React css
    using css by download materiall scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './materialize.css';
+import './sass/materialize.scss';
 import './App.css';
 
 class App extends React.Component {
@@ -8,7 +8,7 @@ class App extends React.Component {
       <div>
         <nav>
           <div className="nav-wrapper">
-            <div>임성규_react_cdn_navigation 적용하기</div>
+            <div>임성규_react_cdn_navigation 적용하기!!</div>
           </div>
         </nav>
         <h1>material css</h1>
